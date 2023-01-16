bf4d0b50130feeab68cd419daf36ebeaf4597471
tarlan-code <tu769djc3@code.edu.az>
2023-01-16 22:53:50 +0400

    added not refresh script

--- Pronia/ViewModels/User/UserLoginVM.cs
+++ Pronia/ViewModels/User/UserLoginVM.cs
@@ -4,7 +4,9 @@ namespace Pronia.ViewModels
 {
     public class UserLoginVM
     {
+        [Required]
         public string UsernameOrEmail { get; set; }
+        [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
         public bool IsPresistance { get; set; }
